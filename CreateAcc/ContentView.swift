59f1a13ee7efa96253c414c4d6526be9bf01a327
Adam <admay202@gmail.com>
2024-07-25 15:41:48 -0500

    there was some issues i fixed

--- CreateAcc/ContentView.swift
+++ CreateAcc/ContentView.swift
@@ -12,72 +12,72 @@ struct ContentView: View {
     @State var isLoginMode = false
     @State var email = ""
     @State var password = ""
-    
-    var body: some View {
-        NavigationView {
-            ScrollView {
-                
-                VStack(spacing: 16) {
-                    Picker(selection: $isLoginMode, label:
-                        Text("Picker here")) {
-                        Text("Login")
-                            .tag(true)
-                        Text("Create Account")
-                            .tag(false)
-                    }.pickerStyle(SegmentedPickerStyle())
+        
+        var body: some View {
+            NavigationView {
+                ScrollView {
                     
-                    if !isLoginMode {
-                        Button {
+                    VStack(spacing: 16){
+                        Picker(selection: $isLoginMode, label: Text("Picker here")) {
+                            Text("Login")
+                                .tag(true)
+                            Text("Create Account")
+                                .tag(false)
+                        }.pickerStyle(SegmentedPickerStyle())
                             
+                        if !isLoginMode {
+                            Button {
+                                
+                            } label: {
+                                Image(systemName: "person.fill")
+                                    .font(.system(size: 64))
+                                    .padding()
+                            }
+                        }
+                        
+                        
+                        Group {
+                            TextField("Email", text: $email)
+                                .keyboardType(.emailAddress)
+                                .autocapitalization(/*@START_MENU_TOKEN@*/.none/*@END_MENU_TOKEN@*/)
+                            
+                            
+                            SecureField("Password", text: $password)
+                        }.padding(12)
+                         .background(Color.white)
+                        
+                        Button {
+                            handleAction()
                         } label: {
-                            Image(systemName: "person.fill")
-                                .font(.system(size: 64))
-                                .padding()
+                            HStack {
+                                
+                                Spacer()
+                                Text(isLoginMode ? "Log In" : "Create Account")
+                                    .foregroundColor(.white)
+                                    .padding(.vertical, 10)
+                                    .font(.system(size: 14, weight: .semibold))
+                                Spacer()
+                            }.background(Color.blue)
                         }
                     }
+                    .padding()
                     
-                    
-                    Group {
-                        TextField("Email", text: $email)
-                            .keyboardType(.emailAddress)
-                            .autocapitalization(.none)
-                        
-                        
-                        SecureField("Password", text: $password)
-                    }
-                    .padding(12)
-                    .background(Color.white)
-                    
-                    Button{
-                        handelAction()
-                    } label: {
-                        HStack {
-                            Spacer()
-                            Text(isLoginMode ? "Log In" : "Create Account")
-                                .foregroundColor(.white)
-                                .padding(.vertical, 10)
-                                .font(.system(size: 14, weight:.semibold))
-                            Spacer()
-                        }.background(Color.blue)
-                        
-                    }
                 }
-                .padding()
+                .navigationTitle(isLoginMode ? "Login" : "Create Account")
+                .background(Color(.init(white: 0, alpha: 0.05))
+                    .ignoresSafeArea())
+                
             }
-            .navigationTitle(isLoginMode ? "Log In" : "Create Account")
-            .background(Color(.init(white: 0, alpha: 0.05))
-                .ignoresSafeArea())
+        }
+        
+        private func handleAction() {
+            if isLoginMode {
+                print("Should log into Firebase with existing credentials")
+            } else {
+                print("Register a new account inside of Firebase Auth and then store image in Storage somehow....")
+            }
         }
     }
-    
-    private func handelAction() {
-        if isLoginMode {
-            print("Should log into Firebase with existing credentials")
-        } else {
-            print("Register a new account inside of Firebase Auth and then store image in Storage somehow...")
-        }
-    }
-}
 
 #Preview {
     ContentView()
