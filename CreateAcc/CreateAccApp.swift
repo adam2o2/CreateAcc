1ef3d10ca93d7e4acab62296366b5740c9e9be34
Adam <admay202@gmail.com>
2024-07-25 12:12:23 -0500

    Initial Commit

---
+++ CreateAcc/CreateAccApp.swift
@@ -0,0 +1,17 @@
+//
+//  CreateAccApp.swift
+//  CreateAcc
+//
+//  Created by Adam May on 7/25/24.
+//
+
+import SwiftUI
+
+@main
+struct CreateAccApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
